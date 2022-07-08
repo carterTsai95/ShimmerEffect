f6e5f3c2b8a674f19722a830c7e7445ad8e7e4a7
Hung-Chun(Carter) Tsai <carterTsai95@gmail.com>
2022-07-08 16:14:51 -0400

    Initialize project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Hung-Chun Tsai on 2022-07-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  Shared
+//
+//  Created by Hung-Chun Tsai on 2022-07-08.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
